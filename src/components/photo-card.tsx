7731f6c70a2d6e9c47d94912f14d49321fc47a85
Alex <alex.devenrgy@gmail.com>
2025-07-11 10:24:41 +0300

    feat: add photo description

--- src/components/photo-card.tsx
+++ src/components/photo-card.tsx
@@ -11,7 +11,7 @@ export class PhotoCard extends PureComponent<Props> {
 		const { urls, width, height, alt_description } = this.props.data
 
 		return (
-			<div className='flex flex-col border border-highlight-low rounded-3xl overflow-clip'>
+			<div className='flex break-inside-avoid flex-col border border-highlight-low rounded-3xl overflow-clip'>
 				<img
 					className='object-cover w-full h-full'
 					src={urls.regular}
@@ -20,6 +20,10 @@ export class PhotoCard extends PureComponent<Props> {
 					fetchPriority='high'
 					alt={alt_description}
 				/>
+
+				<div className='py-6 px-4 bg-highlight-med text-center'>
+					<p className='first-letter:capitalize text-sm text-balance'>{alt_description}</p>
+				</div>
 			</div>
 		)
 	}
