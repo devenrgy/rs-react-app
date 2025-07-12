25ce7c631d660607146b822395b58d7dc14f22d3
Alex <alex.devenrgy@gmail.com>
2025-07-12 15:34:03 +0300

    refactor: improve request handling logic

--- src/lib/api/requests/get-photos.ts
+++ src/lib/api/requests/get-photos.ts
@@ -1,15 +1,18 @@
-import { api } from '@/lib/api/instance'
-import type { Photo } from '@/types'
+import { BASE_API_URL } from '@/lib/constants'
+import { addUrlParams } from '@/lib/utils'
 
 type Params = {
 	page?: number
 	per_page?: number
+	signal?: AbortSignal
 }
 
-export const getPhotos = async ({ page = 1, per_page = 10 }: Params = {}) => {
-	return api.get<Photo[]>('/photos', {
-		page,
-		per_page,
-		client_id: import.meta.env.VITE_CLIENT_ID
-	})
+const endpoint = '/photos'
+
+export const getPhotos = async ({ page = 1, per_page = 10, signal }: Params = {}) => {
+	const params = { page, per_page, client_id: import.meta.env.VITE_CLIENT_ID }
+
+	const url = addUrlParams(`${BASE_API_URL}${endpoint}`, params)
+
+	return fetch(url, { signal })
 }
