4d45ccf3aab6db7c4381828baf7a571c7d072f12
Alex <alex.devenrgy@gmail.com>
2025-07-12 10:06:42 +0300

    chore: configure vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
+/// <reference types="vitest/config" />
+
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+	test: {
+		coverage: {
+			include: ['src/**/*.{ts,tsx}'],
+			exclude: ['src/**/*.test.{ts,tsx}', 'src/index.{ts,tsx}', 'src/setup-tests.ts', 'src/**/*.d.ts'],
+			thresholds: {
+				statements: 80,
+				branches: 50,
+				functions: 50,
+				lines: 50
+			}
+		},
+		clearMocks: true,
+		globals: true,
+		setupFiles: ['./src/setup-tests.ts'],
+		environment: 'happy-dom',
+		css: true
+	},
 	plugins: [react(), tailwindcss(), tsconfigPaths()]
 })
