1440d57f6a24221b5cc8e4985a676079c8723ed8
Alex <alex.devenrgy@gmail.com>
2025-07-20 01:57:42 +0300

    refactor: simplify interface type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,10 @@
 export type Photo = {
 	id: string
-	slug: string
 	width: number
 	height: number
-	color: string
-	blur_hash: string
-	description: string | null
 	alt_description: string
-	likes: number
 	urls: {
-		raw: string
-		full: string
 		regular: string
-		small: string
-		small_s3: string
-		thumb: string
 	}
 }
 
