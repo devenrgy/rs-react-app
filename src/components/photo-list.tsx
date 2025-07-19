118e32c10b96ffe6ae2fb5e56dae9fe74664783b
Alex <alex.devenrgy@gmail.com>
2025-07-20 01:58:23 +0300

    feat: add items existence check in component

--- src/components/photo-list.tsx
+++ src/components/photo-list.tsx
@@ -4,19 +4,21 @@ import { PhotoCard } from '@/components/photo-card'
 import type { Photo } from '@/types'
 
 interface Props {
-	items: Photo[]
+	items?: Photo[]
 }
 
 export class PhotoList extends Component<Props> {
 	render() {
 		return (
-			<ul className='columns-1 gap-5 space-y-5 sm:columns-2 lg:columns-3 lg:space-y-10 lg:gap-x-10'>
-				{this.props.items.map(item => (
-					<li key={item.id}>
-						<PhotoCard data={item} />
-					</li>
-				))}
-			</ul>
+			this.props.items && (
+				<ul className='columns-1 gap-5 space-y-5 sm:columns-2 lg:columns-3 lg:space-y-10 lg:gap-x-10'>
+					{this.props.items.map(item => (
+						<li key={item.id}>
+							<PhotoCard data={item} />
+						</li>
+					))}
+				</ul>
+			)
 		)
 	}
 }
