279149bac4466f9108e211ec29babdc60bb01da2
Alex <alex.devenrgy@gmail.com>
2025-07-20 01:55:38 +0300

    test: add custom render with context

---
+++ tests/mocks/custom-renders.tsx
@@ -0,0 +1,23 @@
+import { render, type RenderOptions } from '@testing-library/react'
+import type { ReactNode } from 'react'
+
+import { Context, type ContextValue } from '@/provider'
+
+export const mockContext = {
+	handleUpdateSearchQuery: () => null,
+	abortRequest: () => null,
+	fetchPhotos: () => null,
+	items: [],
+	error: null,
+	isLoading: false,
+	handleResetError: () => null,
+	searchQuery: ''
+}
+
+interface CustomRenderOptions extends RenderOptions {
+	props: ContextValue
+}
+
+export const renderWithContextProvider = (ui: ReactNode, { props, ...renderOptions }: CustomRenderOptions) => {
+	return render(<Context.Provider value={props}>{ui}</Context.Provider>, renderOptions)
+}
