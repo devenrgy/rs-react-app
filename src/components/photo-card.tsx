e44801585deb27f487e23529e71172d56b2f2d2d
Alex <alex.devenrgy@gmail.com>
2025-07-27 02:07:26 +0300

    test

--- src/components/photo-card.tsx
+++ src/components/photo-card.tsx
@@ -1,31 +1,25 @@
 import type { Photo } from '@/types'
-import { Link, useLocation } from 'react-router'
 
-type Props = {
+interface Props {
 	data: Photo
 }
 
 export const PhotoCard = ({ data }: Props) => {
-	const { search } = useLocation()
-	const { urls, width, height, alt_description, id } = data
+	const { urls, width, height, alt_description } = data
 
 	return (
-		<figure className='group relative flex flex-col shadow-iris/20 hover:shadow-xl border border-highlight-low rounded-3xl overflow-clip break-inside-avoid duration-200'>
-			<p className='overflow-clip'>
-				<img
-					className='w-full h-full object-cover group-hover:scale-101 duration-200'
-					src={urls.regular}
-					width={width}
-					height={height}
-					fetchPriority='high'
-					alt={alt_description || ''}
-				/>
-			</p>
+		<figure className='flex break-inside-avoid flex-col overflow-clip rounded-3xl border border-highlight-low'>
+			<img
+				className='h-full w-full object-cover'
+				src={urls.regular}
+				width={width}
+				height={height}
+				fetchPriority='high'
+				alt={alt_description}
+			/>
 
-			<figcaption className='flex flex-col gap-5 bg-base px-4 py-6'>
-				<p className='text-sm text-center first-letter:capitalize text-balance'>{alt_description}</p>
-
-				<Link className='before:absolute before:inset-0 self-end bg-pine group-hover:bg-pine/80 px-3 py-2 rounded-full text-xs text-right duration-200' preventScrollReset to={{ pathname: `id=${id}`, search }}>More details</Link>
+			<figcaption className='bg-highlight-med px-4 py-6 text-center'>
+				<p className='text-sm text-balance first-letter:capitalize'>{alt_description}</p>
 			</figcaption>
 		</figure>
 	)
