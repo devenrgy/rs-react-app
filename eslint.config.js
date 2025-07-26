7f4a49eb23144dbb2d6df01c3962ef2563b0ab12
Alex <alex.devenrgy@gmail.com>
2025-07-27 00:32:31 +0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,55 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import reactCompiler from 'eslint-plugin-react-compiler'
-import vitest from '@vitest/eslint-plugin'
-import testingLibrary from 'eslint-plugin-testing-library'
-import jestDom from 'eslint-plugin-jest-dom'
+import antfu from '@antfu/eslint-config'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
-export default tseslint.config(
-	{ ignores: ['dist', 'coverage'] },
-	{
-		extends: [
-			js.configs.recommended,
-			...tseslint.configs.strict,
-			eslintPluginPrettier,
-			testingLibrary.configs['flat/react'],
-			jestDom.configs['flat/recommended']
-		],
-		files: ['**/*.{ts,tsx}'],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser
-		},
-		plugins: {
-			react,
-			vitest,
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
-			'react-compiler': reactCompiler,
-			'simple-import-sort': simpleImportSort
-		},
-		rules: {
-			'no-unused-vars': 'off',
-			...reactHooks.configs.recommended.rules,
-			...vitest.configs.recommended.rules,
-			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-			'react-compiler/react-compiler': 'error',
-			'simple-import-sort/imports': 'error',
-			'simple-import-sort/exports': 'error',
-			...react.configs.recommended.rules,
-			...react.configs['jsx-runtime'].rules,
-			'testing-library/no-node-access': 'off'
-		},
-		settings: {
-			react: {
-				version: 'detect'
-			}
-		}
-	}
-)
+export default antfu({
+	react: true,
+	typescript: true,
+
+	lessOpinionated: true,
+	isInEditor: false,
+
+	stylistic: {
+		indent: 'tab',
+		quotes: 'single',
+	},
+
+	formatters: {
+		css: true,
+	},
+
+	ignores: [
+		'migrations/**/*',
+	],
+}, jsxA11y.flatConfigs.recommended, {
+}, {
+	rules: {
+		'antfu/top-level-function': 'off',
+		'antfu/no-top-level-await': 'off',
+		'style/jsx-quotes': ['error', 'prefer-single'],
+		'style/brace-style': ['error', '1tbs'],
+		'ts/no-require-imports': 'off',
+		'ts/consistent-type-definitions': ['error', 'type'],
+		'react/prefer-destructuring-assignment': 'off',
+		'node/prefer-global/process': 'off',
+	},
+})
