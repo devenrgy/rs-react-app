450a68e9eb398f1a5f285769e956efcf8e454924
Alex <alex.devenrgy@gmail.com>
2025-07-27 00:25:00 +0300

    refactor: move vitest config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 /// <reference types="vitest/config" />
+
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [react(), tailwindcss(), tsconfigPaths()]
+	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	test: {
+		coverage: {
+			enabled: true,
+			include: ['src/**/*.{ts,tsx}'],
+			exclude: ['src/index.{ts,tsx}', 'src/**/*.d.ts', 'src/main.tsx', 'src/config/**'],
+			thresholds: {
+				statements: 80,
+				branches: 50,
+				functions: 50,
+				lines: 50,
+			},
+		},
+		clearMocks: true,
+		globals: true,
+		setupFiles: ['tests/vitest.setup.ts'],
+		environment: 'happy-dom',
+		css: true,
+	},
 })
