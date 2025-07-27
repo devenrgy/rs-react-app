ddf5674f83101f3d144266bf088a5c2bc7c3cbdc
Alex <alex.devenrgy@gmail.com>
2025-07-27 11:09:05 +0300

    feat: add ThemeProvider

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,29 @@
+import { createContext, type ReactNode, useContext, useState } from 'react'
+
+type Theme = 'light' | 'dark'
+type ThemeContextType = {
+	theme: Theme
+	toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType | null>(null)
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+	const [theme, setTheme] = useState<Theme>('light')
+
+	const toggleTheme = () => {
+		setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'))
+	}
+
+	return <ThemeContext value={{ theme, toggleTheme }}>{children}</ThemeContext>
+}
+
+export const useThemeContext = () => {
+	const context = useContext(ThemeContext)
+
+	if (!context) {
+		throw new Error('useThemeContext must be used within a ThemeProvider')
+	}
+
+	return context
+}
