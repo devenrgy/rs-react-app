9faca4005204d2b47563577fbfa26d45b9ff0de6
Alex <alex.devenrgy@gmail.com>
2025-07-27 21:48:30 +0300

    test: update snapshots

--- tests/components/not-found.test.tsx
+++ tests/components/not-found.test.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react'
 import type { ReactNode } from 'react'
 import { setupWithRouter } from 'tests/vitest.setup'
-import { expect, expect } from 'vitest'
+import { expect } from 'vitest'
 
 import { NotFound } from '@/components/not-found'
 
