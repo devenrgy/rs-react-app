6b5be2d50da52bc45966ca57c76116efef2d1eca
Alex <alex.devenrgy@gmail.com>
2025-07-27 13:47:18 +0300

    chore: setup routes config

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -1,8 +1,10 @@
-import { createBrowserRouter } from 'react-router'
+import { createBrowserRouter, redirect } from 'react-router'
 
 import { NotFound } from '@/components/not-found'
+import { PhotoDetails } from '@/components/photo-details'
+import { HomeLayout } from '@/layouts/home-layout'
+import { getPhoto } from '@/lib/api/requests/get-photo'
 import { About } from '@/pages/about'
-import { Home } from '@/pages/home'
 import { Root } from '@/pages/root'
 
 export const routes = createBrowserRouter([
@@ -11,7 +13,23 @@ export const routes = createBrowserRouter([
 		children: [
 			{
 				path: '/',
-				Component: Home
+				Component: HomeLayout,
+				children: [
+					{
+						path: '/:id',
+						loader: async ({ params }) => {
+							const response = await getPhoto({ id: params?.id })
+							const data = await response.json()
+
+							if (data.errors) {
+								return redirect('/404')
+							}
+
+							return data
+						},
+						Component: PhotoDetails
+					}
+				]
 			},
 			{
 				path: '/about',
