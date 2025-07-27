8bc9bb74a56f4c77531e6ea6d4673fef4d5115dc
Alex <alex.devenrgy@gmail.com>
2025-07-27 13:51:05 +0300

    test: add setupWithRouter

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,13 +1,11 @@
 import '@testing-library/jest-dom/vitest'
 
-import { render, type RenderOptions } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import type { ReactNode } from 'react'
-
-import { type ContextValue } from '@/provider'
+import { BrowserRouter } from 'react-router'
 
 import { server } from './mocks/api'
-import { renderWithContextProvider } from './mocks/custom-renders'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterEach(() => server.resetHandlers())
@@ -18,11 +16,11 @@ export const setup = (ui: ReactNode) => ({
 	...render(ui)
 })
 
-interface CustomRenderOptions extends RenderOptions {
-	props: ContextValue
-}
+export const setupWithRouter = (ui: ReactNode, { route = '/' } = {}) => {
+	window.history.pushState({}, 'Test page', route)
 
-export const setupWithContextProvider = (ui: ReactNode, props: CustomRenderOptions) => ({
-	user: userEvent.setup(),
-	...renderWithContextProvider(ui, props)
-})
+	return {
+		user: userEvent.setup(),
+		...render(ui, { wrapper: BrowserRouter })
+	}
+}
