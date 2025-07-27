f55e3a92387f5c5a121ce455f3ec0fda5890542f
Alex <alex.devenrgy@gmail.com>
2025-07-27 13:48:39 +0300

    feat: add interface Photo

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,125 @@
-export type Photo = {
+interface Collection {
 	id: string
-	width: number
-	height: number
-	alt_description: string
-	urls: {
-		regular: string
+	title: string
+	description: string | null
+	published_at: string
+	updated_at: string
+	curated: boolean
+	featured: boolean
+	total_photos: number
+	private: boolean
+	share_key: string
+}
+
+interface Breadcrumb {
+	slug: string
+	title: string
+	index: number
+	type: string
+}
+
+interface User {
+	id: string
+	username: string
+	name: string
+	first_name: string
+	last_name: string | null
+	bio: string
+	location: string
+	portfolio_url: string
+	instagram_username: string
+	twitter_username: string
+	total_collections: number
+	total_likes: number
+	total_photos: number
+	total_promoted_photos: number
+	accepted_tos: boolean
+	for_hire: boolean
+	profile_image: {
+		small: string
+		medium: string
+		large: string
+	}
+	links: {
+		self: string
+		html: string
+		photos: string
+		likes: string
+		portfolio: string
+	}
+	social: {
+		instagram_username: string
+		portfolio_url: string
+		twitter_username: string
+		paypal_email: string | null
 	}
 }
 
-export type SearchPhotosResponse = {
-	results: Photo[]
+interface PhotoLinks {
+	self: string
+	html: string
+	download: string
+	download_location: string
+}
+
+interface Sponsorship {
+	impression_urls: string[]
+	tagline: string
+	tagline_url: string
+	sponsor: User
+}
+
+interface TopicSubmissions {
+	travel?: {
+		status: string
+		approved_on: string
+	}
+}
+
+export interface Photo {
+	id: string
+	slug: string
+	created_at: string
+	updated_at: string
+	promoted_at: string | null
+	width: number
+	height: number
+	color: string
+	blur_hash: string
+	description: string
+	alt_description: string
+	urls: {
+		raw: string
+		full: string
+		regular: string
+		small: string
+		thumb: string
+		small_s3: string
+	}
+	links: PhotoLinks
+	likes: number
+	liked_by_user: boolean
+	current_user_collections: Collection[]
+	sponsorship: Sponsorship
+	topic_submissions: TopicSubmissions
+	user: User
+	breadcrumbs: Breadcrumb[]
+	asset_type: string
+	alternative_slugs: {
+		en: string
+		es: string
+		ja: string
+		fr: string
+		it: string
+		ko: string
+		de: string
+		pt: string
+		id: string
+	}
+}
+
+export interface SearchPhotosResponse {
 	total: number
 	total_pages: number
+	results: Photo[]
 }
