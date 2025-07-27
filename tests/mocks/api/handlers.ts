070e3d15ea2a514936b8d7542bf5789d8a30f9cd
Alex <alex.devenrgy@gmail.com>
2025-07-27 18:09:58 +0300

    refactor: correct handlers

--- tests/mocks/api/handlers.ts
+++ tests/mocks/api/handlers.ts
@@ -1,36 +1,39 @@
 import { http, HttpResponse } from 'msw'
 
-import { BASE_API_URL } from '@/configs/constants'
+import { API_PHOTO_URL, API_SEARCH_URL } from '@/configs/constants'
+
+import { mockPhoto, mockSearchPhotosResponse } from './data'
 
 export const handlers = [
-	http.get(`${BASE_API_URL}/photos`, ({ request }) => {
+	http.get(`${API_PHOTO_URL}/:id`, ({ request, params }) => {
 		const url = new URL(request.url)
+		const { id } = params
+		const client_id = url.searchParams.get('client_id')
 
-		const page = url.searchParams.get('page')
-		const per_page = url.searchParams.get('per_page')
+		if (!client_id) {
+			return new HttpResponse(null, { status: 401, statusText: 'Missing client_id' })
+		}
 
-		return HttpResponse.json({ page, per_page })
+		if (!id) {
+			return new HttpResponse(null, { status: 400, statusText: 'Invalid ID' })
+		}
+
+		return HttpResponse.json({ ...mockPhoto, id })
 	}),
 
-	http.get(`${BASE_API_URL}/search/photos`, ({ request }) => {
+	http.get(`${API_SEARCH_URL}`, ({ request }) => {
 		const url = new URL(request.url)
-
-		const page = url.searchParams.get('page')
-		const per_page = url.searchParams.get('per_page')
 		const query = url.searchParams.get('query')
+		const client_id = url.searchParams.get('client_id')
 
-		return HttpResponse.json({ query, per_page, page })
+		if (!client_id) {
+			return new HttpResponse(null, { status: 401, statusText: 'Missing client_id' })
+		}
+
+		if (!query) {
+			return new HttpResponse(null, { status: 400, statusText: 'Missing query' })
+		}
+
+		return HttpResponse.json(mockSearchPhotosResponse)
 	})
 ]
-
-// http.get('/resource', () => {
-//   return new HttpResponse(null, { status: 404 })
-// })
-//
-//
-// server.use(
-//     http.get('/user', () => {
-//       return new HttpResponse(null, { status: 500 })
-//     })
-//   )
-//
