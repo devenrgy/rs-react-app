a10fe0ea981cde5f5ac8520e0d4551e3fc47c8f3
Alex <alex.devenrgy@gmail.com>
2025-07-29 13:05:50 +0300

    chore: replace vitest config with vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,24 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [react(), tailwindcss(), tsconfigPaths()]
+	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	test: {
+		// reporters: ['default', 'html'],
+		coverage: {
+			enabled: true,
+			include: ['src/**/*.{ts,tsx}'],
+			exclude: ['src/index.{ts,tsx}', 'src/**/*.d.ts', 'src/main.tsx', 'src/configs/**'],
+			thresholds: {
+				statements: 80,
+				branches: 50,
+				functions: 50,
+				lines: 50
+			}
+		},
+		clearMocks: true,
+		globals: true,
+		setupFiles: ['tests/vitest.setup.ts'],
+		environment: 'happy-dom',
+		css: true
+	}
 })
