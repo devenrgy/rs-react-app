ef29cad63c931e34f5b74d9a95e182480ff739a2
Alex <alex.devenrgy@gmail.com>
2025-08-08 12:12:13 +0300

    test: ✅ add root test

--- tests/pages/root.test.tsx
+++ tests/pages/root.test.tsx
@@ -1,3 +1,4 @@
+import { QueryClient } from '@tanstack/react-query'
 import { screen } from '@testing-library/react'
 import { createElement } from 'react'
 import { createRoutesStub } from 'react-router'
@@ -8,6 +9,7 @@ import { PhotoDetails } from '@/components/photo-details'
 import { Spinner } from '@/components/spinner'
 import { DEFAULT_PAGE, DEFAULT_QUERY } from '@/configs/constants'
 import { HomeLayout } from '@/layouts/home-layout'
+import { loader as homeLoader } from '@/pages/home'
 import { Root } from '@/pages/root'
 
 describe('Root', () => {
@@ -43,10 +45,45 @@ describe('Root', () => {
 		const heading1 = await screen.findByRole('heading', { name: DEFAULT_QUERY })
 		const heading2 = await screen.findByRole('heading', { level: 2, name: mockPhoto.alt_description })
 
-		screen.debug()
-
 		expect(header).toBeInTheDocument()
 		expect(heading1).toBeInTheDocument()
 		expect(heading2).toBeInTheDocument()
+	})
+
+	it('should render Root page content correctly with searchInput value', async () => {
+		const queryClient = new QueryClient()
+
+		const RootStub = createRoutesStub([
+			{
+				Component: Root,
+				children: [
+					{
+						path: '/',
+						id: 'home',
+						loader: homeLoader(queryClient),
+						Component: HomeLayout,
+						hydrateFallbackElement: createElement(Spinner),
+						children: [
+							{
+								path: '/:id',
+								loader: () => ({}),
+								Component: PhotoDetails
+							}
+						]
+					}
+				]
+			}
+		])
+
+		const { user } = render(<RootStub initialEntries={['/']} />)
+
+		const searchInput = await screen.findByRole('textbox', { name: /search/i })
+
+		await user.type(searchInput, 'Dog')
+		await user.keyboard('{Enter}')
+
+		const heading = await screen.findByRole('heading', { name: /dog/i })
+
+		expect(heading).toHaveTextContent(/dog/i)
 	})
 })
