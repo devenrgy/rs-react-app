8fb428456a0661d7458d6f9eb1fc6bc85f79ff4c
Alex <alex.devenrgy@gmail.com>
2025-08-17 03:05:00 +0300

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const config: NextConfig = {
 	images: {
 		remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
 	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 }
 
 export default withNextIntl(config)
