9c8931d37d1357d5e68d6449eb54a1d06a12fe8d
Alex <alex.devenrgy@gmail.com>
2025-08-17 18:07:58 +0300

    refactor: add constants

--- src/views/feed-photo/ui/feed-photo-page.tsx
+++ src/views/feed-photo/ui/feed-photo-page.tsx
@@ -1,6 +1,7 @@
 import type { Locale } from 'next-intl'
 import { setRequestLocale } from 'next-intl/server'
 import { Suspense } from 'react'
+import { DEFAULT_PAGE, DEFAULT_QUERY } from '@/shared/config/constants'
 import { BaseLayout } from '@/shared/ui/base-layout'
 import { getSearchPhotos } from '../api/get-search-photos'
 import { FeedPhotoList } from './feed-photo-list'
@@ -14,8 +15,8 @@ type Props = {
 export default async function FeedPhotoPage(props: Props) {
 	const searchParams = await props.searchParams
 	const { locale } = await props.params
-	const query = searchParams?.query || 'Pine'
-	const currentPage = Number(searchParams?.page) || 1
+	const query = searchParams?.query || DEFAULT_QUERY
+	const currentPage = Number(searchParams?.page) || DEFAULT_PAGE
 	const getSearchPhotosPromise = getSearchPhotos({ query, page: currentPage })
 
 	setRequestLocale(locale)
